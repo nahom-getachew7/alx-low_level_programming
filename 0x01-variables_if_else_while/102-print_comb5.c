4bf001e65674219463c1e814c98cbb5568bd6db6
nahom-getachew7 <ets050012feng@aastu.edu.et>
2022-03-13 03:17:30 -0700

    edited advance

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,44 +8,43 @@
  */
 int main(void)
 {
-	int c;
-	int d;
-	int e;
-	int f = 0;
+int c;
+int d;
+int e;
+int f = 0;
 
-	while (f < 10)
+while (f < 10)
+{
+	e = 0;
+	while (e < 10)
 	{
-	        e = 0;
-	        while (e < 10)
-        	{
-                	d = 0;
-                	while (d < 10)
-                	{
-                        	c = 0;
-                        	while (c < 10)
-                        	{
-                                	if (!(f == c && e == d))
-                                	{
-                                        	putchar('0' + f);
-                                        	putchar('0' + e);
-                                        	putchar(' ');
-                                        	putchar('0' + d);
-                                        	putchar('0' + c);
-                                        	if (!(f + e == 18 && c + d == 17 && d == 9))
-                                        	{
-                                                	putchar(',');
-                                                	putchar(' ');
-                                        	}
-                                	}
-                                	c++;
-                        	}
-                        	d++;
-                	}
-                	e++;
-        	}
-        	f++;
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (!(f == c && e == d))
+				{
+					putchar('0' + f);
+					putchar('0' + e);
+					putchar(' ');
+					putchar('0' + d);
+					putchar('0' + c);
+					if (!(f + e == 18 && c + d == 17 && d == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
 	}
-	putchar('\n');
-	return (0);
+	f++;
 }
-
+putchar('\n');
+return (0);
+}
