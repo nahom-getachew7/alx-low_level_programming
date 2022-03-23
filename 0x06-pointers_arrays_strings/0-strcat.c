936f10681175e5b352e16d23d5a81ffdb56d50ae
nahom-getachew7 <ets050012feng@aastu.edu.et>
2022-03-23 12:33:42 -0700

    Q0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,41 +1,26 @@
 #include "main.h"
-
 /**
- * _strcat - concatenates two strings
- * @dest: string one
- * @src: string two
- * Return: concatenated string
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
-
 char *_strcat(char *dest, char *src)
 {
+	int count = 0, count2 = 0;
 
-	int i = 0;
-	int j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0')
+	while (*(dest + count) != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		count++;
 	}
-	dest[i] = '\0';
 
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
-/* Another way of doing it */
-/*
-	
-	   char *_strcat(char *dest, char *src)
-           {
-               int dest_len = strlen(dest);
-               int i;
-	       
-               for (i = 0 ; i < n && src[i] != '\0' ; i++)
-                   dest[dest_len + i] = src[i];
-               dest[dest_len + i] = '\0';
-               return dest;
-           }
-*/
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,79 +1,18 @@
-#ifndef _MAIN
-#define _MAIN
+#ifndef _main_H_
+#define _main_H_
 
-/**
- * _putchar - puts character to standard output
- * @c: character to put to standard output
- */
-
-int _putchar(char c);
 char *_strcat(char *dest, char *src);
-/**
- * _strcat - concatenate n bytes to destination string
- * @dest: string to be appended to
- * @src: string to append
- * @n: append n number of bytes(chars)
- * Return: concatenated string
- */
-
 char *_strncat(char *dest, char *src, int n);
-
-/**
- * _strncpy - copies n bytes of a source into buffer of a destination string,
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * @n: n bytes to be copied
- * Return: copied string
- */
-
 char *_strncpy(char *dest, char *src, int n);
-
-/**
- * _strcmp - compare two strings
- * @s1: one string
- * @s2: one string
- * Return: int that tells num spaces in between
- */
-
 int _strcmp(char *s1, char *s2);
-
-/**
- * reverse_array - reverse array elements
- * @a: array
- * @n: number of elements in array
- */
-
 void reverse_array(int *a, int n);
-
-/**
- * string_toupper - capitalize all letters in string
- * @s: string to manipulate
- * Return: string with all letters capitalized
- */
-
 char *string_toupper(char *);
-
-/**
- * string_toupper - capitalize first letter of all words
- * @s: string to manipulate
- * Return: string
- */
-
-char *cap_string(char *s);
-
-/**
- * rot13 - encode string using rot13
- * @s: string to encode to rot13
- * Return: encoded string
- */
-
-char *rot13(char *s);
-
-/**
- * print_number - print numbers chars
- * @n: integer
- * Return: 0
- **/
-
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
